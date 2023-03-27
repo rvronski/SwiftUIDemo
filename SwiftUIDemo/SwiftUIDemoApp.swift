7b57096ed2c7885076941457885a9281cc22adcf
rvronski <rvronski@gmail.com>
2023-03-27 23:07:00 +0500

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by ROMAN VRONSKY on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
